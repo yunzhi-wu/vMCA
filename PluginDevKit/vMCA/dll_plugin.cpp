9ae6c604e040c42fc7edda2eb96b163115c0b8a0
Yunzhi <yunzhi.wu@outlook.com>
2016-08-01 16:50:10 +0200

    add known event array for a better display

--- PluginDevKit/vMCA/dll_plugin.cpp
+++ PluginDevKit/vMCA/dll_plugin.cpp
@@ -15,6 +15,67 @@
 #include "plugin_text_parser.h"
 #include "dll_plugin.h"
 
+#define KNOWN_EVENTS_NUM 57
+static std::string known_events[KNOWN_EVENTS_NUM] =
+{
+	"XioWrite",
+	"UpMeFoFindThread",
+	"ULL1PE_PUCCH_COORD_handleGraphDoneThr",
+	"ULL1PE_PUCCH_COORD_handleCellBufferReadyIndThr",
+	"ULL1PE_PUCCH_COORD_handleAllocationIndThr",
+	"PucchPrbSlot1",
+	"PucchPrbSlot0",
+	"PucchPrbSframe",
+	"PucchFoPrep",
+	"PucchAllocJobPrbProcsBuffers",
+	"ULL1PE_COMMON_globalEvHndlThr",
+	"ULL1PE_CELL_COORD_handleSubframeDoneIndThr",
+	"HarqDecoding",
+	"ConvDecoderCQI",
+	"Deint",
+	"SignalMeas",
+	"RiDecoding",
+	"PuschConstructor",
+	"NoiseMeas2",
+	"NoiseMeas1",
+	"meas2DlDone",
+	"FODPrepS",
+	"DecPreProcSlave",
+	"SoftDemap_1",
+	"SoftDemap_0",
+	"AntCombFde_1",
+	"AntCombFde_0",
+	"ChanEst5E_1",
+	"ChanEst5E_0",
+	"ChanEst4E_1",
+	"ChanEst4E_0",
+	"PostCe3_1",
+	"PostCe3_0",
+	"ChanEst3_1_1",
+	"ChanEst3_1_0",
+	"ChanEst3_0_1",
+	"ChanEst3_0_0",
+	"ChanEst2SectMeas_1",
+	"ChanEst2SectMeas_0",
+	"ChanEst2_1_1",
+	"ChanEst2_1_0",
+	"ChanEst2_0_1",
+	"ChanEst2_0_0",
+	"ChanEst1SectMeas_1",
+	"ChanEst1SectMeas_0",
+	"TimeAdv",
+	"ChanEst1_1_1",
+	"ChanEst1_1_0",
+	"ChanEst1_0_1",
+	"ChanEst1_0_0",
+	"RSGenSlot_1",
+	"RSGenSlot_0",
+	"bbiTrace_hitTable_thread",
+	"bbiMcsUpdaterThread",
+	"bbiMcsBufferHandlerThread",
+	"bbiCommStackMsg_rxThread",
+	"bbiCommStackLinx_rxThread",
+};
 
 CPlugin_DLL_API_V1* DLL_API_Factory(void)
 {
@@ -46,7 +107,7 @@ CPlot_LPP_Ziplog::CPlot_LPP_Ziplog()
   SetSubPlotProperties(m_subPlotID_Sequence, SUB_PLOT_PROPERTY_SEQUENCE);
 
   m_lifeLines_number = 0;
-  m_events_number = 1000000;
+  m_events_number = 0;
 }
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------
@@ -86,7 +147,7 @@ void CPlot_LPP_Ziplog::pvPlotRow(const char* row_p, const unsigned int* length_p
 
   */
 
-#define MAX_EVENT_NAME_LEN (32)
+#define MAX_EVENT_NAME_LEN (64)
   char event_name[MAX_EVENT_NAME_LEN];
 
   m_parser.SetText(row_p, *length_p);
@@ -121,28 +182,41 @@ void CPlot_LPP_Ziplog::pvPlotRow(const char* row_p, const unsigned int* length_p
   
   std::map<std::string, int>::iterator it;
   it = m_events_dict.find(event_name);
-  m_events_number++;
   if (it == m_events_dict.end())
   {
 	  if (m_lifeLines_number < MAX_NUM_OF_LIFE_LINES - 1)
 	  {
 		  m_lifeLines_number++;
+		  int lifeLineIndex = 0;
+		  for (; lifeLineIndex < KNOWN_EVENTS_NUM; lifeLineIndex++)
+		  {
+			  if (!strcmp(event_name, known_events[lifeLineIndex].c_str()))
+			  {
+				  break;
+			  }
+		  }
+		  if (lifeLineIndex == KNOWN_EVENTS_NUM)
+		  {
+			  // unknown event
+			  lifeLineIndex += m_events_number;
+			  m_events_number++;
+		  }
+		  lifeLineIndex += 1;
+		  m_events_dict.insert(std::pair<std::string, int>(event_name, lifeLineIndex));
 
-		  m_events_dict.insert(std::pair<std::string, int>(event_name, m_lifeLines_number));
-
-		  m_lifeLines_a[m_lifeLines_number] = m_sequenceDiagram_p->AddLifeLine((float)m_lifeLines_number - 0.25f,
-			  (float)m_lifeLines_number + 0.25f, event_name, (unsigned int)strlen(event_name),
-			  RGB(0x55, 20 + 20 * m_lifeLines_number, 20 + 20 * m_lifeLines_number));
+		  m_lifeLines_a[lifeLineIndex] = m_sequenceDiagram_p->AddLifeLine((float)lifeLineIndex - 0.25f,
+			  (float)lifeLineIndex + 0.25f, event_name, (unsigned int)strlen(event_name),
+			  RGB(0x55, 20 + 20 * lifeLineIndex, 20 + 20 * lifeLineIndex));
 
 //		  m_sequenceDiagram_p->AddEvent(m_lifeLines_a[m_lifeLines_number], m_events_number, rowIndex, NULL, 0, RGB(90, 90, 90), false);
-		  m_sequenceDiagram_p->AddEvent(m_lifeLines_a[m_lifeLines_number], event_time_d, rowIndex, NULL, 0, RGB(90, 90, 90), false);
+		  m_sequenceDiagram_p->AddEvent(m_lifeLines_a[lifeLineIndex], event_time_d, rowIndex, NULL, 0, RGB(90, 90, 90), false);
 	  }
   }
   else
   {
-	  int lifeLineDest = it->second;
+	  int lifeLineIndex = it->second;
 //	  m_sequenceDiagram_p->AddEvent(m_lifeLines_a[lifeLineDest], m_events_number, rowIndex, NULL, 0, RGB(90, 90, 90), false);
-	  m_sequenceDiagram_p->AddEvent(m_lifeLines_a[lifeLineDest], event_time_d, rowIndex, NULL, 0, RGB(90, 90, 90), false);
+	  m_sequenceDiagram_p->AddEvent(m_lifeLines_a[lifeLineIndex], event_time_d, rowIndex, NULL, 0, RGB(90, 90, 90), false);
   }
 }
 
